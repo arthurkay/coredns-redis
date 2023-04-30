a508369873f301accbbf1c75c2e35793e811a967
Arthur Kalikiti <arthur@kalikiti.net>
2023-05-01 00:56:04 +0200

    initial commit

---
+++ setup.go
@@ -0,0 +1,119 @@
+package redis
+
+import (
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+	"strconv"
+	"time"
+)
+
+// Register the plugin
+func init() {
+	plugin.Register("redis", setup)
+}
+
+func redisParse(c *caddy.Controller) (*Redis, error) {
+	redis := Redis {
+		keyPrefix:"",
+		keySuffix:"",
+		Ttl:300,
+		zoneUpdateTime: 10*time.Minute,
+	}
+	var (
+		err            error
+	)
+
+	for c.Next() {
+		if c.NextBlock() {
+			for {
+				switch c.Val() {
+				case "address":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.redisAddress = c.Val()
+				case "password":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.redisPassword = c.Val()
+				case "prefix":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.keyPrefix = c.Val()
+				case "suffix":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.keySuffix = c.Val()
+				case "connect_timeout":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.connectTimeout, err = strconv.Atoi(c.Val())
+					if err != nil {
+						redis.connectTimeout = 0
+					}
+				case "read_timeout":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					redis.readTimeout, err = strconv.Atoi(c.Val())
+					if err != nil {
+						redis.readTimeout = 0;
+					}
+				case "zone_update_time":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					zoneUpdateTime, err := strconv.Atoi(c.Val())
+					if err != nil {
+						redis.zoneUpdateTime = 10 * time.Minute;
+					}
+					redis.zoneUpdateTime = time.Duration(zoneUpdateTime) * time.Second
+				case "ttl":
+					if !c.NextArg() {
+						return &Redis{}, c.ArgErr()
+					}
+					var val int
+					val, err = strconv.Atoi(c.Val())
+					if err != nil {
+						val = defaultTtl
+					}
+					redis.Ttl = uint32(val)
+				default:
+					if c.Val() != "}" {
+						return &Redis{}, c.Errf("unknown property '%s'", c.Val())
+					}
+				}
+
+				if !c.Next() {
+					break
+				}
+			}
+
+		}
+
+		redis.Connect()
+		redis.LoadZones()
+
+		return &redis, nil
+	}
+	return &Redis{}, nil
+}
+
+func setup(c *caddy.Controller) error {
+	r, err := redisParse(c)
+	if err != nil {
+		return plugin.Error("redis", err)
+	}
+
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		r.Next = next
+		return r
+	})
+
+	return nil
+}
